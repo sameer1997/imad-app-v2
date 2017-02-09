97463517c39c57383d9e9d09f30647bfb454237a
sameer1997 <201552078@iiitvadodara.ac.in>
2017-02-09 15:47:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/article_one', function (req, res) {
 });
 
 app.get('/article_two', function (req, res) {
-    res.send("article two is served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
 });
 
 app.get('/article_three', function (req, res) {
